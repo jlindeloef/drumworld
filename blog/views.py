092220e83a591ea8865b59f1371a7b7b8bb9367d
Johan Lindelöf <j_lindeloef@hotmail.com>
2023-11-17 00:22:58 +0000

    Adding a likesection in post.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from .models import Post
 from .forms import CommentForm
 from django.views.generic import ListView
+from django.http import HttpResponseRedirect
+
 
 class PostList(generic.ListView):
     model = Post
@@ -64,15 +66,16 @@ class PostDetail(View):
             },
         )
 
-class CatListView(ListView):
-    template_name = 'category.html'
-    context_object_name = 'catlist'
 
-    def get_queryset(self):
-        content = {
-            'cat': self.kwargs['category'],
-            'posts': Post.objects.filter(
-                category__name=self.kwargs['category']).filter(status='published')
-        }
-        return content
+class PostLike(View):
+    
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
 
