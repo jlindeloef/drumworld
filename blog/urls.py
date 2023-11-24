1fcfd56885eb51a6aeaaf663305a1991959194cf
Johan Lindelöf <j_lindeloef@hotmail.com>
2023-11-24 21:44:20 +0000

    Adding new style to footer and sign out.

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('category/<category>/', views.CatListView.as_view(), name='category'),
-    path('edit/<int:pk>', CommentUpdateView.as_view(), name='edit_comment'),
+    path('edit/<int:pk>/', CommentUpdateView.as_view(), name='edit_comment'),
     path('delete/<int:pk>/', CommentDeleteView.as_view(),name='delete_comment'),
 ]
