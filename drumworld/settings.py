4cf72d9360db3a48a30104a5482d6c6f011e55e5
Johan Lindelöf <j_lindeloef@hotmail.com>
2023-12-01 10:32:09 +0000

    Changing code fore deployment

--- drumworld/settings.py
+++ drumworld/settings.py
@@ -28,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# X_FRAME_OPTIONS = 'SAMEORIGIN'
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['8000-jlindeloef-drumworld-461ouribgxg.ws-eu106.gitpod.io', 'drumworld.herokuapp.com',  'drumworld-9b0612a3caf4.herokuapp.com', 'localhost']
 
